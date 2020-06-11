df2e869e9e1c0f97c4a7963af1f22d6f7173c8bf
jmjmf-iscteiul <jmjmf@iscte-iul.pt>
2020-06-11 16:48:25 +0100

    Giiiiiiiiiiiiiiiiiiiittt

--- src/main/java/app/Utils.java
+++ src/main/java/app/Utils.java
@@ -3,12 +3,16 @@ package app;
 import java.io.*;
 
 import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.lib.*;
 import org.eclipse.jgit.revwalk.*;
 import org.eclipse.jgit.treewalk.*;
 import org.eclipse.jgit.treewalk.filter.*;
 
 public class Utils {
+	Git git;
 	Repository repository;
 	
 	/*
@@ -19,20 +23,37 @@ public class Utils {
 	 */
 
 	void accessGit() {
-		try {
-			Git git = Git.open(new File("https://github.com/vbasto-iscte/ESII1920.git"));
-			//TODO isto é bola assim
-			repository = git.getRepository();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if(repository == null) {
+			try {
+				git = Git.cloneRepository()
+				  .setURI("https://github.com/vbasto-iscte/ESII1920.git")
+				  .setDirectory(new File("/erepo65465456456456546fff545"))
+				  .call();
+				//TODO isto é bola assim
+				repository = git.getRepository();
+			} catch (InvalidRemoteException e) {
+				System.err.println("Error - Invalid Remote " + e);
+				e.printStackTrace();
+			} catch (TransportException e) {
+				System.err.println("Error - Transport " + e);
+				e.printStackTrace();
+			} catch (GitAPIException e) {
+				System.err.println("Error - GitAPI " + e);
+				e.printStackTrace();
+			}
 		}
-
+		else {
+			//TODO fazer pull, also meter isto inteligente :)
+		}
 	}
 
 	void readFile() {
+		accessGit();
 		// a RevWalk allows to walk over commits based on some filtering that is defined
 		try {
+			System.out.println(repository.getBranch());
 			ObjectId lastCommitId = repository.resolve(Constants.HEAD);
+			System.out.println(lastCommitId.toString());
 			RevWalk revWalk = new RevWalk(repository);
 			RevCommit commit = revWalk.parseCommit(lastCommitId);
 			// and using commit's tree find the path
@@ -54,12 +75,12 @@ public class Utils {
 
 				// and then one can the loader to read the file
 				loader.copyTo(System.out);
-			} catch (Exception e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			revWalk.dispose();
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
