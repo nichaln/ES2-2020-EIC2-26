bc8478c688f9b628220ede4344ac3b541b584eb8
jmjmf-iscteiul <jmjmf@iscte-iul.pt>
2020-06-12 02:16:34 +0100

    Yay ficheiro a sacar :)

--- src/main/java/app/Utils.java
+++ src/main/java/app/Utils.java
@@ -16,20 +16,24 @@ public class Utils {
 	Repository repository;
 	
 	/*
-	 * Tentar aceder ao ficheiro daqui para todos usarmos :)
-	 * 
-	 * 
+	 * Consegui aceder ao ficheiro daqui para todos usarmos :)
 	 * 
 	 */
 
 	void accessGit() {
-		if(git == null) {
+		File rep = new File("/Repositorio");
+		if (rep.exists()) { // Repository exists, opening, and 
+			try {
+				git = Git.open(new File("/Repositorio/.git"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else { // Repository doesn't exist, going to create it.
 			try {
 				git = Git.cloneRepository()
 				  .setURI("https://github.com/vbasto-iscte/ESII1920.git")
-				  .setDirectory(new File("/ESIIGrupo26000000000000000000000000000"))
+				  .setDirectory(new File("/Repositorio"))
 				  .call();
-				//TODO isto é bué assim
 			} catch (InvalidRemoteException e) {
 				System.err.println("Error - Invalid Remote " + e);
 				e.printStackTrace();
@@ -40,9 +44,7 @@ public class Utils {
 				System.err.println("Error - GitAPI " + e);
 				e.printStackTrace();
 			}
-		}
-		else {
-			//TODO fazer pull, also meter isto inteligente :)
+		
 		}
 		repository = git.getRepository();
 	}
@@ -51,15 +53,12 @@ public class Utils {
 		accessGit();
 		// a RevWalk allows to walk over commits based on some filtering that is defined
 		try {
-			System.out.println(repository.getBranch());
+			//System.out.println(repository.getBranch());
 			ObjectId lastCommitId = repository.resolve(Constants.HEAD);
-			System.out.println(lastCommitId.toString());
 			RevWalk revWalk = new RevWalk(repository);
 			RevCommit commit = revWalk.parseCommit(lastCommitId);
 			// and using commit's tree find the path
 			RevTree tree = commit.getTree();
-			System.out.println("Having tree: " + tree);
-
 			// now try to find a specific file
 			try {
 				TreeWalk treeWalk = new TreeWalk(repository);
@@ -67,20 +66,24 @@ public class Utils {
 				treeWalk.setRecursive(true);
 				treeWalk.setFilter(PathFilter.create("covid19spreading.rdf"));
 				if (!treeWalk.next()) {
+					treeWalk.close();
 					throw new IllegalStateException("Did not find expected file 'covid19spreading.rdf'");
 				}
 
 				ObjectId objectId = treeWalk.getObjectId(0);
 				ObjectLoader loader = repository.open(objectId);
-
 				// and then one can the loader to read the file
 				//loader.copyTo(System.out);
-				System.out.println(loader.toString());
+				byte[] bytes = loader.getBytes();
+				FileOutputStream fos = new FileOutputStream("covid19spreading.rdf");
+				fos.write(bytes);
+				fos.close();
+				treeWalk.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
-			revWalk.dispose();
+			
+			revWalk.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
