e0f0a01c9afe9ee29c3469fee26dea2ccb68bad8
hapgr-iscteiul <hapgr@iscte-iul.pt>
2020-06-12 22:15:14 +0100

    Ponto C) da parte 4 feita. Alterações na maneira como o pom.xml funciona

--- src/main/java/app/Utils.java
+++ src/main/java/app/Utils.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.io.*;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -21,10 +22,36 @@ public class Utils {
 	Repository repository;
 	List<ObjectId> ids = new LinkedList<ObjectId>();
 	List<Ref> call;
+	List<fileInformation> informations = new LinkedList<fileInformation>();
+	
 	/*
 	 * Consegui aceder ao ficheiro daqui para todos usarmos :)
 	 * 
 	 */
+	
+	//Classe nova para ser mais facil de passar informação sobre os ficheiros para fora
+	public class fileInformation {
+		Date timestamp;
+		String fileName, tagName, tagDescription;		
+		public fileInformation(Date timestamp, String filename, String tagName, String tagDescription) {
+			this.timestamp = timestamp;
+			this.fileName = filename;
+			this.tagName = tagName;
+			this.tagDescription = tagDescription;
+		}
+		public Date getTimestamp() {
+			return timestamp;
+		}
+		public String getFileName() {
+			return fileName;
+		}
+		public String getTagName() {
+			return tagName;
+		}
+		public String getTagDescription() {
+			return tagDescription;
+		}
+	}
 
 	void accessGit() {
 		File rep = new File("/Repositorio");
@@ -73,12 +100,23 @@ public class Utils {
 			
 			//Usar este ID para obter o ficheiro master mas não é preciso
 			ObjectId lastCommitId = repository.resolve(Constants.HEAD);
-
 			RevWalk revWalk = new RevWalk(repository);
 			//Percorrer a lista criada de tags, e procurar commits com o ID das tags
 			for(int i = 0; i<ids.size(); i++) {
 				//Encontrar o commit com id da tag i
 				RevCommit commit = revWalk.parseCommit(ids.get(i));
+				
+				//Testes para parte 4
+				System.out.println("-----------------------------------------------");
+				PersonIdent author = commit.getAuthorIdent();
+				System.out.println("Data commit -> " + author.getWhen()); //Usar isto para campo File Timestamp
+				System.out.println("File Tag -> " + call.get(i).getName());
+				System.out.println("Tag Description -> " + commit.getShortMessage()); //Usar isto na tag description?
+				Date timestamp = author.getWhen();
+				String fileTag = call.get(i).getName();
+				String tagDescription = commit.getShortMessage();
+				//Fim de testes
+				
 				RevTree tree = commit.getTree();
 				// e depois faz o download do covid19spreading.rdf
 			try {
@@ -97,7 +135,15 @@ public class Utils {
 				//loader.copyTo(System.out);
 				byte[] bytes = loader.getBytes();
 				FileOutputStream fos = new FileOutputStream("covid19spreading"+i+".rdf");
-				System.out.println("Vou escrever o " + fos.toString());
+				
+				//Testes para parte 4
+				System.out.println("File Name -> covid19spreading"+i+".rdf" );
+				System.out.println("Spread visualizer -> NAO HA CRL");
+				String fileName = "covid19spreading"+i+".rdf";
+				informations.add(new fileInformation(timestamp, fileName, fileTag, tagDescription ));
+				//Fim de testes
+				
+				//System.out.println("Vou escrever o " + fos.toString());
 				fos.write(bytes);
 				fos.close();
 				treeWalk.close();
@@ -111,6 +157,24 @@ public class Utils {
 		}
 
 	}
+	
+	
+	public List<fileInformation> getInformations() {
+		try {
+			readFile();
+		} catch (RevisionSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoHeadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return informations;
+	}
+
 	public static void main(String[] args) {
 		try {
 			new Utils().readFile();
--- src/main/java/covid_graph_spread/CovidGraphSpread.java
+++ src/main/java/covid_graph_spread/CovidGraphSpread.java
@@ -1,14 +1,87 @@
 package covid_graph_spread;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import app.Utils;
+import app.Utils.fileInformation;
+import pl.edu.icm.cermine.ContentExtractor;
+import pl.edu.icm.cermine.exception.AnalysisException;
+import pl.edu.icm.cermine.metadata.model.DateType;
+import pl.edu.icm.cermine.metadata.model.DocumentMetadata;
+import pl.edu.icm.cermine.tools.timeout.TimeoutException;
 
 public class CovidGraphSpread {
 	
+	Utils informations = new Utils();
 	
-
+	ArrayList<String> lines = new ArrayList<String>();
+	
+	public void writeHTML() {
+		FileWriter fWriter = null;
+		BufferedWriter writer = null;
+		try {
+		    fWriter = new FileWriter("C:\\Users\\katsa\\git\\ES2-2020-EIC2-26\\src\\main\\java\\covid_graph_spread\\webSpreadTable.html");
+		    writer = new BufferedWriter(fWriter);
+		    writer.write("<!DOCTYPE html>\r\n" + 
+		    		"<html>\r\n" + 
+		    		"<head>\r\n" + 
+		    		"<meta charset=\"ISO-8859-1\">\r\n" + 
+		    		"<title>COVID discoveries</title>\r\n" + 
+		    		"</head>\r\n" + 
+		    		"\r\n" + 
+		    		"<style>\r\n" + 
+		    		"table, th, td {\r\n" + 
+		    		"  border: 1px solid black;\r\n" + 
+		    		"}\r\n" + 
+		    		"\r\n" + 
+		    		"</style>\r\n" + 
+		    		"<body>\r\n" + 
+		    		"\r\n" + 
+		    		"<table style=\"width:60%\">\r\n" + 
+		    		"  <tr>\r\n" + 
+		    		"    <th>File timestamp</th>\r\n" +
+		    		"    <th>File Name</th>\r\n" + 
+		    		"    <th>File Tag</th>\r\n" + 
+		    		"    <th>Tag Description</th>\r\n" + 
+		    		"    <th>Spread Visualization Link</th>\r\n" +
+		    		"  </tr>\r\n" + 
+		    		getLines() +
+		    		"</table>\r\n" + 
+		    		"</body>\r\n" + 
+		    		"</html>");
+		    writer.close();
+		    
+		} catch (Exception e) {
+		  //catch any exceptions here
+		}
+	}
+	
+	public String getLines() {
+		List<fileInformation> files = informations.getInformations();
+		String codigoHtml = "";
+		for (int i = 0; i<files.size(); i++) {
+			String linha = "  <tr>\r\n" + 
+	    		   	"<td>" + files.get(i).getTimestamp() +"</td>\r\n" + 
+	    		"    <td>"+ files.get(i).getFileName() +"</td>\r\n" + 
+	    		"    <td>"+ files.get(i).getTagName() +"</td>\r\n" +
+	    		"    <td>"+ files.get(i).getTagDescription() +"</td>\r\n" +  
+	    		"    <td> por completar </td>\r\n" + 
+	    		"  </tr>\r\n";
+			codigoHtml += " " + linha;
+		}
+		return codigoHtml;
+	}
 	
 	public static void main(String[] args) {
-		
+		CovidGraphSpread test = new CovidGraphSpread();
+		test.writeHTML();
+		System.out.println(test.getLines());
 	}
 
 }
