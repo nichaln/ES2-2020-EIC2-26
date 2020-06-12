ebb2e683f68f60a2ec46a355b16ba9da1cc9399a
hapgr-iscteiul <hapgr@iscte-iul.pt>
2020-06-12 18:17:56 +0100

    Mudado o utils.java de modo a ir buscar os ficheiros com TAGS

--- src/main/java/app/Utils.java
+++ src/main/java/app/Utils.java
@@ -1,11 +1,16 @@
 package app;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jgit.api.*;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.eclipse.jgit.lib.*;
 import org.eclipse.jgit.revwalk.*;
 import org.eclipse.jgit.treewalk.*;
@@ -14,7 +19,8 @@ import org.eclipse.jgit.treewalk.filter.*;
 public class Utils {
 	Git git;
 	Repository repository;
-	
+	List<ObjectId> ids = new LinkedList<ObjectId>();
+	List<Ref> call;
 	/*
 	 * Consegui aceder ao ficheiro daqui para todos usarmos :)
 	 * 
@@ -49,17 +55,32 @@ public class Utils {
 		repository = git.getRepository();
 	}
 
-	void readFile() {
+	void readFile() throws RevisionSyntaxException, NoHeadException, GitAPIException {
 		accessGit();
+	
+		
+		//Buscar referencias para commits com tags e criar uma lista com todas as tags
+		call = git.tagList().call();
+		for (Ref ref : call) {
+		    System.out.println("Tag: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName());
+		    ids.add(ref.getObjectId());
+		}
+		
+		//System.out.println(tags.toString());
 		// a RevWalk allows to walk over commits based on some filtering that is defined
 		try {
 			//System.out.println(repository.getBranch());
+			
+			//Usar este ID para obter o ficheiro master mas não é preciso
 			ObjectId lastCommitId = repository.resolve(Constants.HEAD);
+
 			RevWalk revWalk = new RevWalk(repository);
-			RevCommit commit = revWalk.parseCommit(lastCommitId);
-			// and using commit's tree find the path
-			RevTree tree = commit.getTree();
-			// now try to find a specific file
+			//Percorrer a lista criada de tags, e procurar commits com o ID das tags
+			for(int i = 0; i<ids.size(); i++) {
+				//Encontrar o commit com id da tag i
+				RevCommit commit = revWalk.parseCommit(ids.get(i));
+				RevTree tree = commit.getTree();
+				// e depois faz o download do covid19spreading.rdf
 			try {
 				TreeWalk treeWalk = new TreeWalk(repository);
 				treeWalk.addTree(tree);
@@ -75,13 +96,14 @@ public class Utils {
 				// and then one can the loader to read the file
 				//loader.copyTo(System.out);
 				byte[] bytes = loader.getBytes();
-				FileOutputStream fos = new FileOutputStream("covid19spreading.rdf");
+				FileOutputStream fos = new FileOutputStream("covid19spreading"+i+".rdf");
+				System.out.println("Vou escrever o " + fos.toString());
 				fos.write(bytes);
 				fos.close();
 				treeWalk.close();
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
+			}}
 			
 			revWalk.close();
 		} catch (IOException e) {
@@ -90,6 +112,17 @@ public class Utils {
 
 	}
 	public static void main(String[] args) {
-		new Utils().readFile();
+		try {
+			new Utils().readFile();
+		} catch (RevisionSyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoHeadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
