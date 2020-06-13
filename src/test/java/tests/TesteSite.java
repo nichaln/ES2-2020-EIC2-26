9fc5080632a3de139a30acb35a5cae67536252e1
nanok-iscteiul <nanok@iscte-iul.pt>
2020-06-13 14:20:54 +0100

    p

--- src/test/java/tests/TesteSite.java
+++ src/test/java/tests/TesteSite.java
@@ -3,17 +3,26 @@ package tests;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
+import javax.mail.Flags;
+import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.search.FlagTerm;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -26,11 +35,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TesteSite {
-	static WebDriver driver;
-	static Properties p;
-	static String email;
-	static String messageContent;
-	static LinkedList<UrlStatus> urlsStatus;
+	private static final int SLEEP_AFTER_URL = 550;
+	private static WebDriver driver;
+	private static Properties p;
+	private static String email;
+	private static String messageContent;
+	private static LinkedList<UrlStatus> urlsStatus;
 	
 	public class UrlStatus{
 		public String url;
@@ -97,22 +107,24 @@ public class TesteSite {
 		}
 //		sendEmail();
 		writeHtmlFile();
+		clearInbox();
 	}
 
 	private void processLink(String type) {
 		String url = p.getProperty(type + "_url");
 		String pagename = p.getProperty(type + "_pagename");
 		driver.get(url);
+		
 		UrlStatus urlstatus = new UrlStatus(url);
 		
 		if (driver.getTitle().equals(pagename)) {
 			System.out.println("all good");
-			urlstatus.setStatus("OK");
+			urlstatus.setStatus("<b style=\"color:#00ff00\">OK</b>");
 		} else { // send email erro a aceder à pagina
 			messageContent+="\n Erro ao aceder à pagina: \""+ url + "\". Nome da página esperado: "+ "\""+pagename+"\", Nome obtido: " +
 			"\""+ driver.getTitle()+"\".\n";
 			System.out.println(messageContent);
-			urlstatus.setStatus("DOWN");
+			urlstatus.setStatus("<b style=\"color:#ff0000\">DOWN</b>");
 		}
 		urlsStatus.add(urlstatus);
 	}
@@ -125,6 +137,10 @@ public class TesteSite {
 		boolean stop=false;
 		driver.get(url);
 		
+		try {
+			Thread.sleep(SLEEP_AFTER_URL);
+		} catch (InterruptedException e) {
+		}
 		
 		WebElement elem = null;
 		String[] inputs = p.getProperty(type+"_inputs").split(",");
@@ -143,35 +159,60 @@ public class TesteSite {
 		System.out.println(driver.getTitle());
 		if (driver.getTitle().equals(resultPage) && !stop) {
 			System.out.println("Sucesso");
-			urlstatus.setStatus("OK");
+			urlstatus.setStatus("<b style=\"color:#00ff00\">OK</b>");
 		}else {
 			messageContent+="\n Erro ao aceder à pagina: \""+ url + "\". Nome da página esperado: "+ "\""+resultPage+"\", Nome obtido: " +
 					"\""+ driver.getTitle()+"\". Numero de inputs introduzidos: "+ inputs.length + ".\n";
 			System.out.println(messageContent);
-			urlstatus.setStatus("DOWN");
+			urlstatus.setStatus("<b style=\"color:#ff0000\">DOWN</b>");
 		}
-		if(p.getProperty(type+"check_email") != null) {
-			UrlStatus us = new UrlStatus(url+"EMAIL AVAILABILITY");
+		if(p.getProperty(type+"_check_email") != null) {
+			UrlStatus us = new UrlStatus(url+" --- EMAIL AVAILABILITY ");
 			try {
 				System.out.println("Aguardando a receção do email!");
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if(check_email_received(p.getProperty(type+"check_email"))) {
-				us.setStatus("EMAIL OK");
+			if(check_email_received(p.getProperty(type+"check_email"), p.getProperty(type+"_check_subject"))) {
+				us.setStatus("<b style=\"color:#00ff00\">EMAIL OK</b>");
 			}else {
-				us.setStatus("EMAIL DOWN");
-				messageContent+="\n Email não recebido!\n";
+				us.setStatus("<b style=\"color:#ff0000\">EMAIL DOWN</b>");
+				messageContent+="\n Email não recebido! Url: "+ url+ "\n";
 			}
 			urlsStatus.add(us);
 		}
 		urlsStatus.add(urlstatus);
 	}
 	
-	private static boolean check_email_received(String email) {
-		//TODO
-		return true;
+	private static boolean check_email_received(String email,String emailTitle) {
+		try {
+            Properties properties = new Properties();
+            properties.put("mail.pop3.starttls.enable", "true");
+            properties.put("mail.pop3.host", "pop.sapo.pt");
+            properties.put("mail.pop3.port", "995");
+            Session emailSession = Session.getDefaultInstance(properties);
+            Store store = emailSession.getStore("pop3s");
+            store.connect("pop.sapo.pt", "es2-2020-eic2-26@sapo.pt", "passwordES2020");
+            Folder emailFolder = store.getFolder("INBOX");
+            emailFolder.open(Folder.READ_ONLY);
+            Flags seen = new Flags(Flags.Flag.RECENT);
+            FlagTerm unseenFlagTerm = new FlagTerm(seen, false);
+//          Message[] messages = emailFolder.getMessages();
+            Message[] messages = emailFolder.search(unseenFlagTerm);
+            System.out.println(messages.length);
+            Message message = messages[messages.length - 1];
+            
+            String titleSeen = message.getSubject();
+            emailFolder.close(false);
+            store.close();
+            return titleSeen.equals(emailTitle);
+
+        } catch (NoSuchProviderException e) {
+        } catch (MessagingException e) {
+        } catch (Exception e) {
+        }
+        return false;
 	}
 
 	private static void sendEmail() {
@@ -211,14 +252,16 @@ public class TesteSite {
 	public void writeHtmlFile() {
 		FileWriter fWriter = null;
 		BufferedWriter writer = null;
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
 		try {
-		    fWriter = new FileWriter("C:\\Users\\nicha\\wordpress2\\html\\urlsanalytics.html");
+		    fWriter = new FileWriter("C:\\Users\\nicha\\wordpress2\\html\\wp-admin\\urlsanalytics.html");
 		    writer = new BufferedWriter(fWriter);
 		    String html = "<!DOCTYPE html>\r\n"+
 				    		"<html>\r\n" + 
 				    		"<head>\r\n" + 
 				    		"<meta charset=\"ISO-8859-1\">\r\n" + 
-				    		"<title>WEB SITE ANALYTICS</title>\r\n" + 
+				    		"<title>Links Avalability</title>\r\n" + 
 				    		"</head>\r\n" + 
 				    		"\r\n" +
 				    		"<style>\r\n"+
@@ -253,12 +296,49 @@ public class TesteSite {
 		    }
 		    			  
 		    html+="</table>\r\n"+
+		          "<p><b style=\"color:Blue\">Last Check: "+dateFormat.format(date)+"<b></p>\r\n"+
 		    	  "</body>\r\n"+
 		    	  "</html>\r\n";
 		    writer.write(html);
 		    writer.close();
 		} catch (Exception e) {
 		}
+	}
+	
+	private void clearInbox() {
+		try 
+	      {
+	         // get the session object
+	         Properties properties = new Properties();
+	         properties.put("mail.store.protocol", "pop3");
+	         properties.put("mail.pop3s.host", "pop.sapo.pt");
+	         properties.put("mail.pop3s.port", "995");
+	         properties.put("mail.pop3.starttls.enable", "true");
+	         Session emailSession = Session.getDefaultInstance(properties);
+	         Store store = emailSession.getStore("pop3s");
+
+	         store.connect("pop.sapo.pt", "es2-2020-eic2-26@sapo.pt", "passwordES2020");
+
+	         Folder emailFolder = store.getFolder("INBOX");
+	         emailFolder.open(Folder.READ_WRITE);
+
+	         Message[] messages = emailFolder.getMessages();
+	         System.out.println("messages.length---" + messages.length);
+	         int counter = 0;
+	         for (int i = 0; i < messages.length; i++) {
+	        	 Message message = messages[i];
+	        	 message.setFlag(Flags.Flag.DELETED, true);
+	        	 counter++;
+	         }
+	         System.out.println("Mensagens apagadas:" + counter);
+	         emailFolder.close(true);
+	         store.close();
+
+	      } catch (NoSuchProviderException e) {
+	         e.printStackTrace();
+	      } catch (MessagingException e) {
+	         e.printStackTrace();
+	      }
 	}
 
 }
