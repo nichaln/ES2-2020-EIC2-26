ea62c331ec5b1f0447ca73f72a09b168849d289c
nanok-iscteiul <nanok@iscte-iul.pt>
2020-06-13 22:12:36 +0100

    Pequenas alteraçoes no testeSite

--- src/test/java/tests/TesteSite.java
+++ src/test/java/tests/TesteSite.java
@@ -3,7 +3,6 @@ package tests;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
