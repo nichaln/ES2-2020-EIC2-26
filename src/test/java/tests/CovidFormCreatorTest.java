e3872d2d3c2a518613f1a8df283c1fbc927ca8db
jmjmf-iscteiul <jmjmf@iscte-iul.pt>
2020-06-15 01:09:45 +0100

    Tests

---
+++ src/test/java/tests/CovidFormCreatorTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+import covid_query.CovidFormCreator;
+
+public class CovidFormCreatorTest {
+
+	@Test
+	public void testCreateHTMLForm() {
+		CovidFormCreator.createHTMLForm();
+	}
+
+}
---
+++ src/test/java/tests/CovidQueryHandlerTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+import covid_query.CovidQueryHandler;
+
+public class CovidQueryHandlerTest {
+
+	@Test
+	public void testMain() {
+		CovidQueryHandler.main(null);
+	}
+
+}
